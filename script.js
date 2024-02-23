caa816f633713519b7209d9dedbe6a6e7896e17b
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-02-23 16:49:14 +0100

    Adding javaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+function spicyBrothDetails(){
+    let spicyBrothDetails = "Spicy broth is a hot soup made with chili peppers, garlic, ginger, and spices in a tasty broth. It's great for soups and noodles.";
+document.getElementById("spicy-broth").innerHTML = spicyBrothDetails;}
+
+function herbalBrothDetails(){
+    let herbalBrothDetails = "Herbal broth is a flavorful soup base made by simmering aromatic herbs like thyme, rosemary, and parsley with vegetables. It adds depth and freshness to dishes like soups and stews.";
+document.getElementById("herbal-broth").innerHTML = herbalBrothDetails;}
