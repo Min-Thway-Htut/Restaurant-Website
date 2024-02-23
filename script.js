dc4abbb43bcd24acad7ae869d44a7d492639a7a3
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-02-23 23:40:16 +0100

    Updating menu details

--- script.js
+++ script.js
@@ -2,6 +2,32 @@ function spicyBrothDetails(){
     let spicyBrothDetails = "Spicy broth is a hot soup made with chili peppers, garlic, ginger, and spices in a tasty broth. It's great for soups and noodles.";
 document.getElementById("spicy-broth").innerHTML = spicyBrothDetails;}
 
-function herbalBrothDetails(){
-    let herbalBrothDetails = "Herbal broth is a flavorful soup base made by simmering aromatic herbs like thyme, rosemary, and parsley with vegetables. It adds depth and freshness to dishes like soups and stews.";
-document.getElementById("herbal-broth").innerHTML = herbalBrothDetails;}
+function mushroomBrothDetails(){
+    let mushroomBrothDetails = "Introducing our savory mushroom broth, a rich fusion of earthy mushrooms and aromatic herbs. Simmered to perfection, it's a comforting elixir that delights the palate with every nourishing sip.";
+    document.getElementById("mushroom-broth").innerHTML = mushroomBrothDetails;
+}
+
+function beefDescription(){
+    let beefDescription = "Savor the richness of our beef broth, made with tender beef and aromatic spices for a comforting, flavorful experience.";
+    document.getElementById("meat-item").innerHTML = beefDescription;
+    }
+
+function lambDescription(){
+        let lambDescription = "Indulge in the comforting warmth of our lamb broth, crafted with succulent lamb and aromatic herbs for a truly satisfying taste experience.";
+        document.getElementById("lamb-menu").innerHTML = lambDescription;
+    }
+
+function porkDescription(){
+        let porkDescription = "Treat yourself to the hearty goodness of our pork broth, simmered to perfection with savory pork and aromatic seasonings, delivering a deliciously comforting taste in every sip.";
+        document.getElementById("pork-menu").innerHTML = porkDescription;
+    }
+
+function chickenDescription(){
+        let chickenDescription = "Delight in the comforting taste of our chicken broth, crafted with tender chicken and aromatic herbs for a soul-warming experience with every sip.";
+        document.getElementById("chicken-menu").innerHTML = chickenDescription;
+    }
+
+function seafoodDescription(){
+        let seafoodDescription = "Savor the ocean's bounty with our seafood broth, brimming with the fresh flavors of the sea and aromatic herbs, creating a delightful and invigorating taste experience.";
+        document.getElementById("seafood-menu").innerHTML = seafoodDescription;
+    }
