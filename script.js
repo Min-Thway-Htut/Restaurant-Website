29f77f0378471beadd4814ae934004c577303d7d
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-03-09 22:31:35 +0100

    Update on shopping cart

--- script.js
+++ script.js
@@ -119,3 +119,15 @@ function selectInput(list){
     inputBox.value = list.innerHTML;
     resultsBox.innerHTML = '';
 }
+
+let iconCart = document.querySelector('.icon-cart');
+let closeCart = document.querySelector('.close');
+let body = document.querySelector('body');
+
+iconCart.addEventListener('click', ()=>{
+    body.classList.toggle('showCart')
+})
+
+closeCart.addEventListener('click', ()=> {
+    body.classList.toggle('showCart')
+})
